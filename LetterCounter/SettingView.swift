c10d5bc2e9305fe3ec338948f550d7554d993493
Kota1021 <Kota1021@users.noreply.github.com>
2023-07-29 16:01:07 +0900

    added Setting View

---
+++ LetterCounter/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  LetterCounter
+//
+//  Created by 松本幸太郎 on 2023/07/29.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
